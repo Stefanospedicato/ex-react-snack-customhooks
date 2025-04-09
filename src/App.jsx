e29690fef17306f6d89e9d4108179edcb4d717c3
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-04-09 11:27:42 +0200

    feat: snack 3 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-// import useCustomPointer from "./useCustomPointer";
+import useCustomPointer from "./assets/useCustomPointer";
 
-// function App() {
-//   const customPointer = useCustomPointer("🔥");
-
-//   return (
-//     <div>
-//       <h1>Sposta il mouse per vedere il cursore personalizzato!</h1>
-//       {customPointer}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import useDate from "./assets/useDate";
 function App() {
-  const { date } = useDate();
+  const customPointer = useCustomPointer("🔥");
+
   return (
     <div>
-      <h1>Data e ora attuali:</h1>
-      <p>{date}</p>
+      <h1>Sposta il mouse per vedere il cursore personalizzato!</h1>
+      {customPointer}
     </div>
   );
 }
+
 export default App;
+
+// import useDate from "./assets/useDate";
+// function App() {
+//   const { date } = useDate();
+//   return (
+//     <div>
+//       <h1>Data e ora attuali:</h1>
+//       <p>{date}</p>
+//     </div>
+//   );
+// }
+// export default App;
 
 // useSwitch:
 // import useSwitch from "./assets/useSwitch";
--- src/assets/useCustomPointer.jsx
+++ src/assets/useCustomPointer.jsx
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const useCustomPointer = () => {
-  const [pointer, setPointer] = useState("");
+const useCustomPointer = (component) => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  return { customPointer, pointer };
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+    document.addEventListener("mousemove", handleMouseMove);
+    return () => document.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: position.y,
+        left: position.x,
+        transform: "translate (-50%,-50%)",
+        cursor: "none",
+      }}
+    >
+      {component}
+    </div>
+  );
 };
 
 export default useCustomPointer;
